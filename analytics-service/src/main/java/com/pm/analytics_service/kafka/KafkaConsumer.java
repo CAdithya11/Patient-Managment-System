02a9d0507ddcf78b4e17c64f87d95ba565d46b1d
CAdithya11 <cadithya13@gmail.com>
2025-07-28 03:04:18 +0530

    feat(analytics-service): add Kafka consumer for patient events with protobuf support
    - Implemented KafkaConsumer to listen on 'patient' topic and parse PatientEvent protobuf messages.
    - Added patient_event.proto definition for PatientEvent schema.
    - Added Dockerfile and updated docker-compose files for analytics-service.

---
+++ analytics-service/src/main/java/com/pm/analytics_service/kafka/KafkaConsumer.java
@@ -0,0 +1,23 @@
+package com.pm.analytics_service.kafka;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Service
+public class KafkaConsumer {
+    private final static Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+    
+    @KafkaListener(topics = "patient",groupId = "analytic-service")
+    public void consumeEvent(byte[] event){
+        try {
+            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+            log.info("Received Patient Event: [patient_id={},patient_name={},patient_email={}",patientEvent.getPatientID(),patientEvent.getName(),patientEvent.getEmail());
+        }catch (InvalidProtocolBufferException e){
+            log.info("Failed to deserialize event {}",e.getMessage());
+        }
+    }
+}
